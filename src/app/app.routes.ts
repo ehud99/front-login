a454381a52d3ee8ff1bfaa240c756706bb5dca1a
ehud <flyupdrums@gmail.com>
2025-11-05 11:15:58 -0500

    se agregó el modulo login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./Auth/login/login').then(m => m.Login)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./Auth/register/register').then(m => m.Register)
+      }
+    ]
+  },
+
+  {
+    path: 'sesion',
+    children: [
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {
+        path: 'welcome',
+        loadComponent: () =>
+          import('./Interface/welcome/welcome').then(m => m.Welcome),
+      },
+      {
+        path: 'admin',
+        loadComponent: () =>
+          import('./Interface/admin/admin').then(m => m.Admin)
+      }
+    ]
+  },
+
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'auth/login' }
+
+];
